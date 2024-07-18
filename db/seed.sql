56356dee62fb135bbc857179e2aefe4de0f5550c
MikesKids <michael.dantzler@gmail.com>
2024-07-17 21:32:35 -0400

    add and connect database

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+\c songs_db
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('This Is My Heart', 'Boyz II Men', 'Motownphilly', 'February 1991', true),
+('Falsetto', 'The Dream', 'Love Hate', 'January 2007', true),
+('Love', 'Musiq', 'Aijuswanaseing', 'January 2000', true);
